559c901929b72d2cc746590501576bfb064d1102
nirupaangelin <nirupaangelin@gmail.com>
2017-07-09 15:51:46 +0530

    Add files via upload

---
+++ odd between intervals.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	
+		public static void main (String[] args) throws java.lang.Exception
+	{
+	
+	int n1,n2;
+	
+	Scanner s=new Scanner(System.in);
+	n1=s.nextInt();
+	n2=s.nextInt();
+	int i;
+
+for(i=n1;i<=n2;i++)
+{
+	if((i%2)!=0)
+	
+System.out.println("numbers are:"+i);
+	
+		
+}
+}}
