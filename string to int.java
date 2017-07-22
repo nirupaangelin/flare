c1d9b2071069a02929fe791dff81a94484a34702
nirupaangelin <nirupaangelin@gmail.com>
2017-07-22 11:23:43 +0530

    Add files via upload

---
+++ string to int.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner s=new Scanner(System.in);
+	String word=s.next();
+	int out=Integer.parseInt(word);
+	System.out.println(out);
+	}
+}
